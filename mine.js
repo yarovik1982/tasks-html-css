230680ccd32433db44a5c4044fb38bd23f9d9c57
yarovik1982 <65862979+yarovik1982@users.noreply.github.com>
2021-01-21 02:06:17 +0300

    Add files via upload

---
+++ mine.js
@@ -0,0 +1,7 @@
+const solutionBtn = document.querySelector("#solution");
+const block = document.querySelector("#block");
+solutionBtn.addEventListener("click", ()=>{
+	block.classList.toggle("active");
+}) 
+
+
